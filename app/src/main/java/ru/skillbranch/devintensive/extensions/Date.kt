1177ece66a92c525eca86cae9614eff3357e9770
jedi12 <jedi12@mail.ru>
2019-06-29 13:53:31 +0700

    hometask_2 with all stars

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -28,12 +30,44 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    val sign = (date.time - this.time).sign
+    val diff = (date.time - this.time).absoluteValue
+    val diffSecond = diff / SECOND
+    val diffMinute = diff / MINUTE
+    val diffHour = diff / HOUR
+    val diffDay = diff / DAY
+
+    return when {
+        diffSecond in (0..1) ->         if (sign < 0) "вот-вот случится" else "только что"
+        diffSecond in (1..45) ->        if (sign < 0) "через несколько секунд" else "несколько секунд назад"
+        diffSecond in (45..75) ->       if (sign < 0) "через минуту" else "минуту назад"
+        diffSecond in (75..45 * 60) ->  if (sign < 0) "через ${toRightTime(diffMinute, TimeUnits.MINUTE)}" else "${toRightTime(diffMinute, TimeUnits.MINUTE)} назад"
+        diffMinute in (45..75) ->       if (sign < 0) "через час" else "час назад"
+        diffMinute in (75..22 * 60) ->  if (sign < 0) "через ${toRightTime(diffHour, TimeUnits.HOUR)}" else "${toRightTime(diffHour, TimeUnits.HOUR)} назад"
+        diffHour in (22..26) ->         if (sign < 0) "через день" else "день назад"
+        diffHour in (26..360 * 24) ->   if (sign < 0) "через ${toRightTime(diffDay, TimeUnits.DAY)}" else "${toRightTime(diffDay, TimeUnits.DAY)} назад"
+        diffDay > 360 ->                if (sign < 0) "более чем через год" else "более года назад"
+
+        else -> "нипанятна"
+    }
 }
 
-enum class TimeUnits {
-    SECOND,
-    MINUTE,
-    HOUR,
-    DAY
+fun toRightTime(value: Long, timeUnit: TimeUnits): String {
+    val lastValue = when (val lastIndex = value.toString().length) {
+        1 -> value.toString().substring(lastIndex - 1, lastIndex).toInt()
+        else -> value.toString().substring(lastIndex - 2, lastIndex).toInt()
+    }
+
+    return when (lastValue) {
+        1 -> "$lastValue ${timeUnit.name1}"
+        in 2..4 -> "$lastValue ${timeUnit.name2}"
+        else -> "$lastValue ${timeUnit.name3}"
+    }
+}
+
+enum class TimeUnits(val name1: String, val name2: String, val name3: String) {
+    SECOND("секунда", "секунды", "секунд"),
+    MINUTE("минута", "минуты", "минут"),
+    HOUR("час", "часа", "часов"),
+    DAY("день", "дня", "дней")
 }
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,20 +1,68 @@
 package ru.skillbranch.devintensive.utils
 
+val symbolMap = mapOf(
+    "а" to "a",
+    "б" to "b",
+    "в" to "v",
+    "г" to "g",
+    "д" to "d",
+    "е" to "e",
+    "ё" to "e",
+    "ж" to "zh",
+    "з" to "z",
+    "и" to "i",
+    "й" to "i",
+    "к" to "k",
+    "л" to "l",
+    "м" to "m",
+    "н" to "n",
+    "о" to "o",
+    "п" to "p",
+    "р" to "r",
+    "с" to "s",
+    "т" to "t",
+    "у" to "u",
+    "ф" to "f",
+    "х" to "h",
+    "ц" to "c",
+    "ч" to "ch",
+    "ш" to "sh",
+    "щ" to "sh",
+    "ъ" to "",
+    "ы" to "i",
+    "ь" to "",
+    "э" to "e",
+    "ю" to "yu",
+    "я" to "ya")
+
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
-        //TODO FIX ME!!!
-        val parts: List<String>? = fullName?.split(" ")
+        val parts: List<String>? = fullName?.trim()?.split(" ")
 
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
-        return firstName to lastName
+        return when (parts?.size) {
+            1 -> if (parts[0].isNotEmpty()) parts.getOrNull(0) to null else null to null
+            2 -> parts.getOrNull(0) to parts.getOrNull(1)
+            else -> null to null
+        }
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return buildString {
+            for (char in payload) {
+                when {
+                    char == ' ' -> append(divider)
+                    char.isUpperCase() -> append(symbolMap[char.toLowerCase().toString()]?.toUpperCase() ?: char)
+                    else -> append(symbolMap[char.toString()] ?: char)
+                }
+            }
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val first: String = (firstName?.trim()?.firstOrNull() ?: "").toString()
+        val last: String = (lastName?.trim()?.firstOrNull() ?: "").toString()
+
+        if (first == "" && last == "") return null
+        return "$first$last".toUpperCase()
     }
 }
