85127c0ca975712722922bb3fea96aae1b7e9d63
jedi12 <jedi12@mail.ru>
2019-07-04 14:55:31 +0700

    hometask_2 truncate correction 2

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -4,8 +4,8 @@ val htmlTagRegex = """<.*?>""".toRegex()
 val moreTwoSpacesRegex = """\s{2,}""".toRegex()
 
 fun String.truncate(count: Int = 16): String {
-    if (this.length <= count) return this
-    return this.substring(0, count).trim() + "..."
+    if (this.trim().length <= count) return this.trim()
+    return this.trim().substring(0, count).trim() + "..."
 }
 
 fun String.stripHtml(): String {
