0abc2bb18120b25868a9c28f39d8bbb81d064b77
jedi12 <jedi12@mail.ru>
2019-07-13 15:02:19 +0700

    hometask_3 try 1

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,40 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlin.math.roundToInt
+
+fun Activity.hideKeyboard() {
+    val view = this.currentFocus
+    view?.let { v ->
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        imm?.let { it.hideSoftInputFromWindow(v.windowToken, 0) }
+    }
+}
+
+fun Activity.getRootView(): View {
+    return findViewById<View>(android.R.id.content)
+}
+fun Context.convertDpToPx(dp: Float): Float {
+    return TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        dp,
+        this.resources.displayMetrics
+    )
+}
+
+fun Activity.isKeyboardOpen(): Boolean {
+    val visibleBounds = Rect()
+    this.getRootView().getWindowVisibleDisplayFrame(visibleBounds)
+    val heightDiff = getRootView().height - visibleBounds.height()
+    val marginOfError = this.convertDpToPx(50F).roundToInt()
+    return heightDiff > marginOfError
+}
+
+fun Activity.isKeyboardClosed(): Boolean {
+    return !this.isKeyboardOpen()
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,81 @@
+package ru.skillbranch.devintensive.models
+
+class Bender(var status: Status = Status.NORMAL, var question: Question = Question.NAME) {
+
+    fun askQuestion(): String = when (question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        val validationResult = question.validate(answer)
+        if (validationResult != null) return "$validationResult\n${question.question}" to status.color
+
+        return if(question.ansvers.contains(answer)) {
+            question = question.nextQuestion()
+            "Отлично - ты справился\n${question.question}" to status.color
+        } else {
+            status = status.nextStatus()
+            "Это неправильный ответ${if (status == Status.NORMAL) ". Давай все по новой" else ""}\n${question.question}" to status.color
+        }
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 255, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+    }
+
+    enum class Question(val question: String, val ansvers: List<String>) {
+        NAME("Как меня зовут?", listOf("Бендер", "Bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+            override fun validate(answer: String): String? {
+                return if (!answer.first().isUpperCase()) "Имя должно начинаться с заглавной буквы" else null
+            }
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+            override fun validate(answer: String): String? {
+                return if (!answer.first().isLowerCase()) "Профессия должна начинаться со строчной буквы" else null
+            }
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun nextQuestion(): Question = BDAY
+            override fun validate(answer: String): String? {
+                return if (answer.contains("""\d+""".toRegex())) "Материал не должен содержать цифр" else null
+            }
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+            override fun validate(answer: String): String? {
+                return if (answer.contains("""\D+""".toRegex())) "Год моего рождения должен содержать только цифры" else null
+            }
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String? {
+                return if (answer.length != 7 || answer.contains("""\D+""".toRegex())) "Серийный номер содержит только цифры, и их 7" else null
+            }
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+            override fun validate(answer: String): String? = null
+        };
+
+        abstract fun nextQuestion(): Question
+        abstract fun validate(answer: String): String?
+    }
+}
